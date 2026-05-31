ce5f5c84c34710ed06c29e90ce288f0ca2d9d9da
kostykrasil <163845822+kostykrasil@users.noreply.github.com>
2026-05-31 21:58:58 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,75 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <algorithm>
+
+template<typename T>
+class BST {
+ public:
+  struct Node {
+    T value;
+    int count;
+    Node* left;
+    Node* right;
+
+    explicit Node(T val) : value(val), count(1),
+                           left(nullptr), right(nullptr) {}
+  };
+
+ private:
+  Node* root;
+
+  Node* insertNode(Node* node, T val) {
+    if (node == nullptr) {
+      return new Node(val);
+    }
+    if (val < node->value) {
+      node->left = insertNode(node->left, val);
+    } else if (val > node->value) {
+      node->right = insertNode(node->right, val);
+    } else {
+      node->count++;
+    }
+    return node;
+  }
+
+  int depthNode(Node* node) {
+    if (node == nullptr) {
+      return 0;
+    }
+    return 1 + std::max(depthNode(node->left), depthNode(node->right));
+  }
+
+  int searchNode(Node* node, T val) {
+    if (node == nullptr) {
+      return 0;
+    }
+    if (val == node->value) {
+      return node->count;
+    }
+    if (val < node->value) {
+      return searchNode(node->left, val);
+    }
+    return searchNode(node->right, val);
+  }
+
+  void destroyTree(Node* node) {
+    if (node != nullptr) {
+      destroyTree(node->left);
+      destroyTree(node->right);
+      delete node;
+    }
+  }
+
+ public:
+  BST() : root(nullptr) {}
+  ~BST() { destroyTree(root); }
+
+  void insert(T val) { root = insertNode(root, val); }
+  int depth() { return depthNode(root); }
+  int search(T val) { return searchNode(root, val); }
+  
+  Node* getRoot() const { return root; }
+};
 
 #endif  // INCLUDE_BST_H_
